29d142f955a151f5eaa990a37c443ddd55f695bf
sudipghosh1728 <115257913+sudipghosh1728@users.noreply.github.com>
2022-11-06 18:13:03 +0530

    Add files via upload

---
+++ list_print_break_even_odd_sum.py
@@ -0,0 +1,20 @@
+lst=[]
+odd_list=[]
+even_list=[]
+n=int(input("Enter how many numbers do you want to take :\t"))
+print("Enter the elements:\n")
+for i in range(0,n):
+    x=int(input(lst))
+    lst.append(x)
+print("The list is :\t",(lst))
+for i in range(0,len(lst)):
+    if (lst[i]%2==0):
+        even_list.append(lst[i])
+        a=[*set(even_list)]
+    else:
+        odd_list.append(lst[i])
+        b=[*set(odd_list)]
+print("Even elements are :\t",(a))
+print("Odd elements are :\t",(b))
+print("Sum of all even elements :\t",sum(a))
+print("Sum of all odd elements :\t",sum(b))
